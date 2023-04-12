3d1ccd826f58226e9c7c375ac63fbbe82b1e67ba
Andrewab <andrewalcideblanchette@gmail.com>
2023-04-12 12:44:25 -0700

    new changes

--- my-app/src/pages/philosophyPapers.tsx
+++ my-app/src/pages/philosophyPapers.tsx
@@ -9,26 +9,27 @@ export default function myPage() {
   
   return (
     <>
+    <div className={philPageStyles.myBackground}>
       <Head>
         <title>Andrews Website</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        
-      </Head>
-      <header className={philPageStyles.header}>
-    <div>My Philosophy Papers</div>
 
-      <Link href='/' className={philPageStyles.backButton}>
-      <div className={philPageStyles.backButtonA}>Home</div>
-      </Link>
+      </Head>
+
+
+
+  <header className={philPageStyles.header}>
+  <Link href='/' className={philPageStyles.backButton}><div className={philPageStyles.backButtonA}>Home</div></Link>
+    <div>My Philosophy Papers</div>
     </header>
     <div className={philPageStyles.leftWall}></div>
     <div className={philPageStyles.rightWall}></div>
     <div className={philPageStyles.paperWrapper}>
       <PaperBlock/>
     </div>
-      
-
+    <div className={philPageStyles.footer}></div>
+    </div>
     </>
   )
 }
