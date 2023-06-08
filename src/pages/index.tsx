dee4911f04dd65a7d9465c5fc0b5ce05603afe55
Andrewab <andrewalcideblanchette@gmail.com>
2023-06-07 21:11:57 -0700

    nessage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
+//import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from'next/link'
 
-const inter = Inter({ subsets: ['latin'] })
+//const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -17,7 +17,16 @@ export default function Home() {
     <header className={styles.header}>
       <div>Welcome to Andrew Blanchettes Website!</div>
     </header>
+    <div className={styles.myNavBar}>
+    <Link href='philosophyPapers'  className={styles.myNavBarButton}>My Philosophy Papers</Link>
+    </div>
+	<div className={styles.waveContainer}>
 
+	<div className={styles.waveRed}></div>
+	<div className={styles.waveGreen}></div>
+	<div className={styles.waveYellow}></div>
+
+	</div>
     </>
-  )
+  );
 }
