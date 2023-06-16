5ef71a1bf66aac4b4352a3ca7051d9259389b6b8
Andrew Blanchette <andrewb@macadamfd.com>
2023-06-16 11:11:33 -0700

    Removed font download from google

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,20 @@ export default function Home() {
       <header>
         <title>Andrews Website</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
             <div className={styles.footer}></div>
-      </header>
+      <div>Welcome to Andrew Blanchettes Website!</div>
+    </header>
+    <div className={styles.myNavBar}>
+    <Link href='philosophyPapers'  className={styles.myNavBarButton}>My Philosophy Papers</Link>
+    </div>
+	<div className={styles.waveContainer}>
+
+	<div className={styles.waveRed}></div>
+	<div className={styles.waveGreen}></div>
+	<div className={styles.waveYellow}></div>
+
+	</div>
     </>
   );
 }
