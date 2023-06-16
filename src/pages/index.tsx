d1c6893a11dbfece34c8ba909cdc8d6be6a96ed4
Andrew Blanchette <andrewb@macadamfd.com>
2023-06-16 11:08:12 -0700

    Removed font download from google

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,6 @@ export default function Home() {
       <Head>
         <title>Andrews Website</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
             <div className={styles.footer}></div>
       </Head>
     </>
