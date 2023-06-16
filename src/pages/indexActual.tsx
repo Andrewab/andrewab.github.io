0b78033ccababbfe3ae0e346edeb8b5aa11c53eb
Andrew Blanchette <andrewb@macadamfd.com>
2023-06-16 11:04:27 -0700

    Removed font download from google

---
+++ src/pages/indexActual.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Link from'next/link'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Andrews Website</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+            <div className={styles.footer}></div>
+      </Head>
+    <header className={styles.header}>
+      <div>Welcome to Andrew Blanchettes Website!</div>
+    </header>
+    <div className={styles.myNavBar}>
+    <Link href='philosophyPapers'  className={styles.myNavBarButton}>My Philosophy Papers</Link>
+    </div>
+	<div className={styles.waveContainer}>
+
+	<div className={styles.waveRed}></div>
+	<div className={styles.waveGreen}></div>
+	<div className={styles.waveYellow}></div>
+
+	</div>
+    </>
+  );
+}
